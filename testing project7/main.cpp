487050767ea9d32d5ab803bdc1fc8f9c77cd67cb
a1880719CY <140379272+a1880719CY@users.noreply.github.com>
2023-10-10 13:45:16 +1030

    testing1

---
+++ testing project7/main.cpp
@@ -0,0 +1,46 @@
+// Demonstrate sprite drawing in SFML
+#include "SFML/Graphics.hpp"
+#include "Tilemap.h"
+
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(1600, 800), "Tilemap");
+
+    // define the level with an array of tile indices
+    const int level[] =
+    {
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+    };
+
+    // create the tilemap from the level definition
+    TileMap map;
+    if (!map.load("texture\\theguddoge.png", sf::Vector2u(400, 400), level, 4, 2))
+        return -1;
+
+    // run the main loop
+    while (window.isOpen())
+    {
+        // handle events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // draw the map
+        window.clear();
+        window.draw(map);
+        window.display();
+    }
+
+    return 0;
+}
